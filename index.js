c4f3015ce5445f7a6b6e1528989404befa8a971c
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-01-31 21:19:47 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
